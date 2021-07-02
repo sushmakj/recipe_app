5ab5192a6efb4d07ac32b47f6901864727c47002
sushmakj <sushmasinghjk@gmail.com>
2021-07-02 18:22:57 +0530

    initial commit_recipe app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demo-app';
+  loadedFeature='recepi';
+  onNavigate(feature:string){
+this.loadedFeature =feature;
+  }
 }
---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,5 @@
+export class Ingredient{
+    // public name:string;
+    // public amount:number;
+    constructor(public name:String,public amount:number){}
+}
